f0187add46c1ace3f54d1684c5649074d82cb629
Diogo Costa <costa.h4evr@gmail.com>
2013-02-24 19:55:03 -0500

    added possibility to place tabs on top or bottom. added android holo dark theme.

--- ui/tab_group.js
+++ ui/tab_group.js
@@ -6,9 +6,22 @@ function(Utils, Scroll, Widget, Footer, ButtonGroup, Button) {
         this._visibleTab = 0;
         this._scroller = null;
         this._tabHolder = null;
-        this._footer = null;
         this._buttonGroup = null;
         return this;
+    }
+    
+    function onButtonTapped(eventName, e) {
+        var target = e.target;
+        while (target && target.className.indexOf("ui-button") === -1) {
+            target = target.parentNode;
+        }
+        
+        if (target) {
+            var tabIndex = Utils.indexOfByProp(this._buttonGroup.buttons, "_el", target);
+            if (tabIndex > -1) {
+                this.visibleTab(tabIndex);
+            }
+        }
     }
     
     TabGroup.extend(Widget, {
@@ -18,7 +31,6 @@ function(Utils, Scroll, Widget, Footer, ButtonGroup, Button) {
         },
         
         onScrollEnd : function (eventName, x, y) {
-            console.log(x, y);
             var page = Math.round(x / this.el().offsetWidth);
             this.visibleTab(page);
             this.update();
@@ -33,7 +45,7 @@ function(Utils, Scroll, Widget, Footer, ButtonGroup, Button) {
                 }
                 
                 if (index != this._visibleTab && this._scroller) {
-                    this._scroller.scrollTo(index * this.el().offsetWidth, 0);
+                    this._scroller.scrollTo(index * this.el().offsetWidth);
                     this._visibleTab = index;
                 }
             
@@ -53,30 +65,22 @@ function(Utils, Scroll, Widget, Footer, ButtonGroup, Button) {
                 this.el().className = "ui-tab-group";
                 this._tabHolder = document.createElement("div");
                 this._tabHolder.className = "ui-fill";
-                this.el().appendChild(this._tabHolder);
+                this._helperHolder = document.createElement("div");
+                this._helperHolder.className = "ui-tab-holder ui-fill";
+                this._helperHolder.appendChild(this._tabHolder);
+                this.el().appendChild(this._helperHolder);
+                this._buttonGroup = document.createElement("div");
+                this._buttonGroup.className = "ui-tab-buttons";
+                this._buttonGroup.buttons = [];
                 
-                this._scroller = new Scroll(this.el(), {
-                    scrollX: true,
-                    scrollY: false,
-                    bouncing: false,
-                    paging: true,
-                    totalMaxTime: 0.25,
-                    disableTouchEvents : false
-                }).on("scroll:end", Utils.bind(TabGroup.prototype.onScrollEnd, this));
-                
-                this._footer = new Footer();
-                this._footer.update();
-                this.el().appendChild(this._footer.el());
-                
-                this._buttonGroup = new ButtonGroup();
-                this._buttonGroup.attr("stretch", true);
-                this._footer.facet("buttons", this._buttonGroup);
-                this._buttonGroup.on("button:pressed", 
-                    Utils.bind(function (eventName, btn) {
-                        var tabIndex = Utils.indexOfByProp(this._buttonGroup.buttons(), "_el", btn);
-                        this.visibleTab(tabIndex);
-                        this.update();
-                    }, this));
+                var position = this.attr("tabPosition") || 'top';
+                if (position === 'top') {
+                    this.el().className += " ui-tab-buttons-top";
+                    this.el().insertBefore(this._buttonGroup, this.el().children[0]);
+                } else {
+                    this.el().className += " ui-tab-buttons-bottom";
+                    this.el().appendChild(this._buttonGroup);
+                }
             }
             
             Utils.Dom.addClass(this.el(), "ui-tab-group");
@@ -84,9 +88,7 @@ function(Utils, Scroll, Widget, Footer, ButtonGroup, Button) {
             if (this._tabs.length > 0) {
                 var tab, btn,
                     tabSize = 100.0 / this._tabs.length;
-
                 this._tabHolder.style.width = this._tabs.length * 100 + "%";
-                
                 for (var i = 0; i < this._tabs.length; ++i) {
                     tab = this._tabs[i];
                     
@@ -99,24 +101,45 @@ function(Utils, Scroll, Widget, Footer, ButtonGroup, Button) {
                         tab.el().style.width = tabSize + "%";
                         tab.el().style.bottom = "0";
                         this._tabHolder.appendChild(tab.el());
-                        this._buttonGroup.addButton((new Button()).attr("style", "vertical"));
+                        
+                        btn = (new Button()).attr("style", "vertical");
+                        btn.update();
+                        this._buttonGroup.appendChild(btn.el());
+                        this._buttonGroup.buttons.push(btn);
                     }
                     
-                    btn = this._buttonGroup.buttons()[i];
+                    btn = this._buttonGroup.buttons[i];
                     btn.attr("title", tab.attr("title")).attr("icon", tab.attr("icon"))
                        .attr("disabled", this._visibleTab !== i);
+                    btn.update();
+                    btn.el().style.width = tabSize + "%";
+                    btn.on("dom:" + (Utils.Dom.supportsTouch() ? "touchstart" : "click"), 
+                            Utils.bind(onButtonTapped, this));
+                    
+                    if (i == this._visibleTab) {
+                        Utils.Dom.addClass(btn.el(), "ui-selected");
+                    } else {
+                        Utils.Dom.removeClass(btn.el(), "ui-selected");
+                    }
                 }
                 
-                this._buttonGroup.attr("selected", this._visibleTab);
-                this._footer.el().style.display = "block";
                 Utils.Dom.removeClass(this._tabHolder, "ui-footer-off");
+                
+                if (!this._scroller) {
+                    this._scroller = new Scroll(this._helperHolder, {
+                        scrollX: true,
+                        scrollY: false,
+                        bouncing: false,
+                        paging: true,
+                        totalMaxTime: 0.25,
+                        disableTouchEvents : false,
+                        hideScroller : true
+                    }).on("scroll:end", Utils.bind(TabGroup.prototype.onScrollEnd, this));
+                }
+                
             } else {
-                this._footer.facet("buttons", null);
-                this._footer.el().style.display = "none";
                 Utils.Dom.addClass(this._tabHolder, "ui-footer-off");
             }
-            
-            this._footer.update();
         }
     });
     
